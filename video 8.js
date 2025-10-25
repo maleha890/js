a02e9aece323e9489bd6c12be5d66325ef293899
maleha890 <malehamajid890@gmail.com>
2025-10-26 01:25:48 +0500

    Add files via upload

---
+++ video 8.js
@@ -0,0 +1,27 @@
+
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+console.log("2">1);
+console.log("02">1);
+console.log(null>0);
+console.log(null==0); //>,<,== working different in JS
+console.log(null>=0);
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+// null undefined not use avoid it
+//===    strict condition
+console.log("2"===2); 
+
+
+
+
+
+
+
+
+
+
